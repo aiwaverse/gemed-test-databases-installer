ff2994b3dce6d81c2123dfcc63a06d771297bf79
Phi <aiwavision@protonmail.com>
2021-12-29 15:10:02 -0300

    Created basic read code

--- src/bin/executable.rs
+++ src/bin/executable.rs
@@ -1,3 +1,8 @@
+use gemed_test_database_installer::installer::bases::Installation;
+
 fn main() {
-    println!{"A test!"};
+    let inst = Installation::new();
+    println!("Installed prod bases: {:?}", inst.prod_bases);
+    println!("Installed test bases: {:?}", inst.test_bases);
+    println!("Available bases: {:?}", inst.available_bases);
 }
---
+++ src/installer/bases.rs
@@ -0,0 +1,48 @@
+extern crate winreg;
+use winreg::enums::*;
+use winreg::RegKey;
+
+pub struct Installation {
+    pub prod_bases: Vec<String>,
+    pub test_bases: Vec<String>,
+    pub available_bases: Vec<String>,
+    pub single_base: bool,
+}
+
+impl Installation {
+    pub fn new() -> Installation {
+        let single_base;
+        let gemed_key = RegKey::predef(HKEY_LOCAL_MACHINE)
+            .open_subkey_with_flags(r"Software\Interprocess\GemedOnco", KEY_READ)
+            .expect("Não foi possível abrir a chave do registro.");
+        let clients: String = match gemed_key.get_value("Clientes") {
+            Ok(res) => {
+                single_base = false;
+                res
+            }
+            Err(_) => {
+                single_base = true;
+                gemed_key
+                    .get_value("Cliente")
+                    .expect("Não foi possível encontrar nem a chave cliente, nem a chave clientes.")
+            }
+        };
+        let installed_bases: Vec<String> = clients.split(',').map(|x| x.to_owned()).collect();
+        let test_bases: Vec<String> = installed_bases
+            .iter()
+            .filter(|x| x.to_lowercase().ends_with("_teste"))
+            .map(|x| x.to_owned())
+            .collect();
+        let prod_bases: Vec<String> = installed_bases
+            .iter()
+            .filter(|b| !test_bases.contains(b))
+            .map(|x| x.to_owned())
+            .collect();
+        let available_bases: Vec<String> = prod_bases
+            .iter()
+            .filter(|x|!test_bases.contains(x))
+            .map(|x|format!("{}_Teste", x))
+            .collect();
+        Installation {prod_bases, test_bases, available_bases, single_base}
+    }
+}
